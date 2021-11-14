2ec15b144e6c391103c654504d61a40be420a64c
zofiadob <z.dobrowolska24@gmail.com>
2021-11-14 17:40:18 +0100

    finishing menu opening

--- js/menu-functions.js
+++ js/menu-functions.js
@@ -13,7 +13,27 @@ function menuOpenClose(){
     }
 
 
-function subMenuOpenClose(){
-    const subMenuState = document.getElementById('subcathergory-box');
-    subMenuState.style.display = 'none' ? subMenuState.style.display = 'block' : subMenuState.style.display = 'none';
+function subMenuOpenClose(num){
+    const subMenuState1 = document.getElementById('subcathergory-box-1');
+    const subMenuState2 = document.getElementById('subcathergory-box-2');
+    const subMenuState3 = document.getElementById('subcathergory-box-3');
+    console.log(subMenuState1.style.display, subMenuState2.style.display, subMenuState3.style.display);
+
+    if (num == '1'){
+        subMenuState1.style.display == '' ? subMenuState1.style.display = 'block' : subMenuState1.style.display = '';
+        subMenuState2.style.display = '';
+        subMenuState3.style.display = '';
+    }
+
+    else if (num == '2'){
+        subMenuState2.style.display == '' ? subMenuState2.style.display = 'block' : subMenuState2.style.display = '';
+        subMenuState1.style.display = '';
+        subMenuState3.style.display = '';
+    }
+
+    else{
+        subMenuState3.style.display == '' ? subMenuState3.style.display = 'block' : subMenuState3.style.display = '';
+        subMenuState2.style.display = '';
+        subMenuState1.style.display = '';
+    }
 }
